ee02d725d4c74adf5abc3487b43953de3a676365
ansible <ansible@ip-172-31-24-40.us-west-2.compute.internal>
2019-02-21 11:20:10 +0000

    newcommit

---
+++ installprom.sh
@@ -0,0 +1,4 @@
+sudo chown -R prometheus:prometheus /home/prometheus
+sudo tar -xzvf /home/prometheus/prometheus-2.2.1.linux-amd64.tar.gz   -C /home/prometheus/
+sudo mv  /home/prometheus/prometheus-2.2.1.linux-amd64  /home/prometheus/prometheus
+sudo chown -R prometheus:prometheus /home/prometheus
