de4aca0432ce641c88f53eab19841ae4c6b4e7cc
ansible <ansible@ip-172-31-24-40.us-west-2.compute.internal>
2019-03-01 05:10:40 +0000

    new

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -1,10 +1,10 @@
-sudo add-apt-repository ppa:webupd8team/java 
-sudo apt-get update 
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update -y
 sudo apt install oracle-java8-set-default -y
 sudo apt install openjdk-8-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
+sudo apt update -y
 sudo mkdir -p /home/jenkins
 sudo useradd jenkins
 sudo usermod -s /bin/bash jenkins
